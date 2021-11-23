a21a239cbe1a8d0e1e2b7dc4f93096f040733aa9
sidhant bahri <sidhantbahri01@gmail.com>
2021-11-23 22:55:22 +0530

    new changes

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -1,7 +1,22 @@
 var mongoose = require('mongoose'),
 Schema = mongoose.Schema;
 
-
+var animalSchema= new Schema({
+    accessToken:String,
+    Icon:String,
+    Name:String,
+    HealthCondition:String,
+    Status:String,
+    Location:{Number,Number}, 
+    OtherInformation:String,
+    FenceDetail:{Number,Number}
+})
+var AllAnimals= new Schema({
+    TotalAnimals: Number,
+    HealthyAnimalsCount:Number,
+    NormalHealthAnimalsCount:Number,
+    CriticalHealthAnimalsCount:Number,
+})
 module.exports = {
     
 };
