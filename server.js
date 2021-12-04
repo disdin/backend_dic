3ff2a98560bf5d4a88b1de4f1090aaa6e910976b
sidhant bahri <sidhantbahri01@gmail.com>
2021-12-04 15:09:04 +0530

    error resolved

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ import onlineAnimals from "./routeFunctions/onlineAnimals.js";
 import animalHealthDetails from "./routeFunctions/animalHealthDetails.js";
 import particularAnimalDetail from "./routeFunctions/particularAnimalDetail.js";
 import allAnimalsAvailable from "./routeFunctions/allAnimalsAvailable.js";
-import { userSignup } from "./routeFunctions/signUp2.js";
+// import { userSignup } from "./routeFunctions/signUp2.js";
 
-app.post("/signup", userSignup); // not working on heroku | working perfeectly locally
+app.post("/signup",signup); // not working on heroku | working perfeectly locally
 app.post("/signin", signin);
 app.get("/totalAnimals", totalAnimals);
 app.get("/allAnimalDetails", allAnimalDetails);
